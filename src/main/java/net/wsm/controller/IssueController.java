91dba3476f4090c5f964561b73dff124331176eb
Joe Bailey <joe.n.bailey8@gmail.com>
2022-05-26 22:26:34 +1000

    added-some-issue-stuff

---
+++ src/main/java/net/wsm/controller/IssueController.java
@@ -0,0 +1,73 @@
+package net.wsm.controller;
+
+import java.nio.ReadOnlyBufferException;
+import java.security.KeyPairGenerator;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+import net.wsm.model.*;
+import net.wsm.repository.IssueRepository;
+import net.wsm.repository.UserRepository;
+
+@Controller
+public class IssueController {
+    private IssueRepository repository = new IssueRepository();
+    private UserRepository userRepos = new UserRepository();
+
+    @RequestMapping(value = "/newIssue", method = RequestMethod.GET)
+    public ModelAndView issue() {
+        return new ModelAndView("NewIssue", "command", new Issue());
+    }
+
+    @RequestMapping(value = "/addIssue", method = RequestMethod.POST)
+    public String addIssue(@ModelAttribute("issue")Issue issue,
+        BindingResult result, ModelMap model) {
+            if (result.hasErrors()) {
+                //return "error";
+                System.out.println("error!");
+            }
+            repository.createIssue(issue);
+            
+            //handler stuff here I guess
+
+            return "issues";
+        }
+
+    @RequestMapping("/test")
+    public String test(Model model) {
+        User[] users = userRepos.getAll();
+        User u = users[0];
+        System.out.println(u.getTokenExp().toString());
+        return "issues";
+    }
+    
+    @RequestMapping("/issues")
+    public String handler(Model model) {
+        Issue[] issues = repository.getAll();
+        User[] users = userRepos.getAll();
+        HashMap<Issue, User> issueMap = new HashMap<>();
+        for (Issue issue : issues) {
+            System.out.println(issue.toString());
+            User u;
+            for (User user : users) {
+                if (issue.getReporterId() == user.getId()) {
+                    issueMap.put(issue, user);
+                }
+            }
+        }
+        model.addAttribute("issueMap", issueMap);
+        model.addAttribute("issues", issues);
+        return "issues";
+    }
+}
